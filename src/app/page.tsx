ef5b42da9dbb6901d7e703498c21956fd351fd58
shin jooyoung <young@staybility.co.kr>
2024-05-23 00:40:38 +0900

    initinal commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,16 @@
+import { Button } from "@/components/ui/button";
+import {
+  Drawer,
+  DrawerClose,
+  DrawerContent,
+  DrawerDescription,
+  DrawerFooter,
+  DrawerHeader,
+  DrawerTitle,
+  DrawerTrigger,
+} from "@/components/ui/drawer";
+import { Input } from "@/components/ui/input";
+
 export default function Home() {
   return (
     <>
@@ -5,6 +18,23 @@ export default function Home() {
         위픽 서비스 프리텐다드
       </div>
       <div className="font-allroundgothic ">Wepik Service All Round Gothic</div>
+      <Button>버튼</Button>
+      <Input></Input>
+      <Drawer>
+        <DrawerTrigger>Open</DrawerTrigger>
+        <DrawerContent>
+          <DrawerHeader>
+            <DrawerTitle>Are you absolutely sure?</DrawerTitle>
+            <DrawerDescription>This action cannot be undone.</DrawerDescription>
+          </DrawerHeader>
+          <DrawerFooter>
+            <Button>Submit</Button>
+            <DrawerClose>
+              <Button variant="outline">Cancel</Button>
+            </DrawerClose>
+          </DrawerFooter>
+        </DrawerContent>
+      </Drawer>
     </>
   );
 }
