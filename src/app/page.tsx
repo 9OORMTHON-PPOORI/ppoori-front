48abab7253f2263605c8de4e24d2d15ba1cf6898
Jiiker <motive46@naver.com>
2024-05-24 04:44:52 +0900

    feat: style 리팩토링

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -26,56 +26,45 @@ export default function Home() {
 
   return (
     <div className="relative">
-      <Progress className="my-8" value={50 * page} />
       {page === 1 && (
         <div>
-          <div className="flex justify-center rounded-[16px]">
-            <div className="relative h-[120px] w-[120px]">
-              <Image
-                className="rounded-[16px] object-cover"
-                src={faker.image.urlLoremFlickr()}
-                alt="관심 대상 아이콘"
-                fill
-                sizes="100vw"
-              />
-            </div>
+          <div className="ml-[37px] mt-[26px] w-full text-left text-[30px] text-lg font-bold">
+            <div>나는</div>
+            <div className="mt-3">누구인가요?</div>
           </div>
-          <div className="my-6 w-full text-center text-lg">
-            관심 대상군을 선택해주세요.
-          </div>
-          <div className="my-8 grid grid-cols-3 place-items-center gap-2 px-4">
+          <div className="my-8 grid grid-cols-2 place-items-center gap-3 px-6">
             <Button
-              className={`h-[80px] w-[100px] rounded-xl text-lg ${target == "대학생" ? "bg-black text-white" : "bg-white text-black hover:text-white"}`}
+              className={`h-[98px] w-[165px] rounded-xl text-lg ${target == "대학생" ? "bg-black text-white" : "bg-white text-black hover:text-white"}`}
               onClick={() => setTarget("대학생")}
             >
               대학생
             </Button>
             <Button
-              className={`h-[80px] w-[100px] rounded-xl text-lg ${target == "취준생" ? "bg-black text-white" : "bg-white text-black hover:text-white"}`}
+              className={`h-[98px] w-[165px] rounded-xl text-lg ${target == "취준생" ? "bg-black text-white" : "bg-white text-black hover:text-white"}`}
               onClick={() => setTarget("취준생")}
             >
               취준생
             </Button>
             <Button
-              className={`h-[80px] w-[100px] rounded-xl text-lg ${target == "재직자" ? "bg-black text-white" : "bg-white text-black hover:text-white"}`}
+              className={`h-[98px] w-[165px] rounded-xl text-lg ${target == "재직자" ? "bg-black text-white" : "bg-white text-black hover:text-white"}`}
               onClick={() => setTarget("재직자")}
             >
               재직자
             </Button>
             <Button
-              className={`h-[80px] w-[100px] rounded-xl text-lg ${target == "신혼부부" ? "bg-black text-white" : "bg-white text-black hover:text-white"}`}
+              className={`h-[98px] w-[165px] rounded-xl text-lg ${target == "신혼부부" ? "bg-black text-white" : "bg-white text-black hover:text-white"}`}
               onClick={() => setTarget("신혼부부")}
             >
               신혼부부
             </Button>
             <Button
-              className={`h-[80px] w-[100px] rounded-xl text-lg ${target == "농,어업인" ? "bg-black text-white" : "bg-white text-black hover:text-white"}`}
+              className={`h-[98px] w-[165px] rounded-xl text-lg ${target == "농,어업인" ? "bg-black text-white" : "bg-white text-black hover:text-white"}`}
               onClick={() => setTarget("농,어업인")}
             >
               농,어업인
             </Button>
             <Button
-              className={`h-[80px] w-[100px] rounded-xl text-lg ${target == "예술가" ? "bg-black text-white" : "bg-white text-black hover:text-white"}`}
+              className={`h-[98px] w-[165px] rounded-xl text-lg ${target == "예술가" ? "bg-black text-white" : "bg-white text-black hover:text-white"}`}
               onClick={() => setTarget("예술가")}
             >
               예술가
@@ -83,7 +72,7 @@ export default function Home() {
           </div>
           <div className="mt-4 flex w-full justify-center">
             <Button
-              className="fixed bottom-24 w-[100px]"
+              className="fixed bottom-24 h-[60px] w-[120px] rounded-full"
               onClick={() => handleNextBtn()}
               disabled={target === ""}
             >
@@ -94,63 +83,81 @@ export default function Home() {
       )}
       {page === 2 && (
         <div>
-          <div className="flex justify-center rounded-[16px]">
-            <div className="relative h-[120px] w-[120px]">
-              <Image
-                className="rounded-[16px] object-cover"
-                src={faker.image.urlLoremFlickr()}
-                alt="관심 대상 아이콘"
-                fill
-                sizes="100vw"
-              />
-            </div>
+          <div className="ml-[37px] mt-[26px] w-full text-left text-[30px] text-lg font-bold">
+            <div>어떤 정책이</div>
+            <div className="mt-3">궁금하세요?</div>
           </div>
-          <div className="mt-6 w-full text-center text-lg">
-            관심사를 선택해주세요.
-          </div>
-          <div className="mt-8 grid grid-cols-2 place-items-center gap-2 px-6">
+          <div className="mt-8 grid grid-cols-1 place-items-center gap-[10px] px-6">
             <Button
-              className={`h-full w-full rounded-xl py-6 text-lg ${interest == "활동지원" ? "bg-black text-white" : "bg-white text-black hover:text-white"}`}
+              className={`h-[112px] w-[342px] rounded-xl py-6 text-lg ${interest == "활동지원" ? "bg-black text-white" : "bg-white text-black hover:text-white"}`}
               onClick={() => setInterest("활동지원")}
             >
-              <div className="grid">
-                <div>활동</div>
-                <div>지원</div>
+              <div className="flex items-center justify-between gap-[52px]">
+                <div className="relative h-[76px] w-[117px]">
+                  <Image
+                    className="object-cover"
+                    src="/images/interestCon1.svg"
+                    alt="활동지원콘"
+                    fill
+                  />
+                </div>
+                <div>활동지원</div>
               </div>
             </Button>
             <Button
-              className={`h-full w-full rounded-xl py-6 text-lg ${interest == "역량개발" ? "bg-black text-white" : "bg-white text-black hover:text-white"}`}
+              className={`h-[112px] w-[342px] rounded-xl py-6 text-lg ${interest == "역량개발" ? "bg-black text-white" : "bg-white text-black hover:text-white"}`}
               onClick={() => setInterest("역량개발")}
             >
-              <div className="grid">
-                <div>역량</div>
-                <div>개발</div>
+              <div className="flex items-center justify-between gap-[52px]">
+                <div className="relative h-[76px] w-[117px]">
+                  <Image
+                    className="object-cover"
+                    src="/images/interestCon1.svg"
+                    alt="활동지원콘"
+                    fill
+                  />
+                </div>
+                <div>역량개발</div>
               </div>
             </Button>
             <Button
-              className={`h-full w-full rounded-xl py-6 text-lg ${interest == "생활지원" ? "bg-black text-white" : "bg-white text-black hover:text-white"}`}
+              className={`h-[112px] w-[342px] rounded-xl py-6 text-lg ${interest == "생활지원" ? "bg-black text-white" : "bg-white text-black hover:text-white"}`}
               onClick={() => setInterest("생활지원")}
             >
-              <div className="grid">
-                <div>생활</div>
-                <div>지원</div>
+              <div className="flex items-center justify-between gap-[52px]">
+                <div className="relative h-[76px] w-[117px]">
+                  <Image
+                    className="object-cover"
+                    src="/images/interestCon1.svg"
+                    alt="활동지원콘"
+                    fill
+                  />
+                </div>
+                <div>생활지원</div>
               </div>
             </Button>
             <Button
-              className={`h-full w-full rounded-xl py-6 text-lg ${interest == "진로지원" ? "bg-black text-white" : "bg-white text-black hover:text-white"}`}
+              className={`h-[112px] w-[342px] rounded-xl py-6 text-lg ${interest == "진로지원" ? "bg-black text-white" : "bg-white text-black hover:text-white"}`}
               onClick={() => setInterest("진로지원")}
             >
-              <div className="grid">
-                <div>진로</div>
-                <div>지원</div>
+              <div className="flex items-center justify-between gap-[52px]">
+                <div className="relative h-[76px] w-[117px]">
+                  <Image
+                    className="object-cover"
+                    src="/images/interestCon1.svg"
+                    alt="활동지원콘"
+                    fill
+                  />
+                </div>
+                <div>진로지원</div>
               </div>
             </Button>
           </div>
           <div className="mt-4 flex w-full justify-center">
             <Button
-              className="fixed bottom-24 w-[100px]"
+              className="mt-[40px] h-[60px] w-[120px] rounded-full"
               onClick={() => handleNextBtn()}
-              disabled={interest === ""}
+              disabled={target === ""}
             >
               완료
             </Button>
