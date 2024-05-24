400518d1a3ff362337c4201de288dc6e35f3878e
shin jooyoung <young@staybility.co.kr>
2024-05-24 10:38:32 +0900

    Style: 스플래쉬 화면 연동

--- src/components/components/splash-screen/splashScreen.tsx
+++ src/components/components/splash-screen/splashScreen.tsx
@@ -4,9 +4,14 @@ import Image from "next/image";
 
 export function SplashScreen() {
   return (
-    <div className="h-screen w-screen">
-      <div>
-        <Image src="" alt="" />
+    <div className="relative h-screen w-full opacity-100 transition-opacity duration-500 ease-in-out">
+      <div className="absolute bottom-0">
+        <Image
+          src="/images/splash.svg"
+          alt="splashIcon1"
+          width={390}
+          height={726}
+        />
       </div>
     </div>
   );
