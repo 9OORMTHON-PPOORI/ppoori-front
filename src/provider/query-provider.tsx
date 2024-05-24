f9abed688ca15f833ec2ec2d208d0640b4a4a283
shin jooyoung <young@staybility.co.kr>
2024-05-24 11:48:34 +0900

    최종 배포

--- src/provider/query-provider.tsx
+++ src/provider/query-provider.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { PropsWithChildren, useState } from "react";
 
 export function ReactQueryProvider({ children }: PropsWithChildren) {
@@ -9,7 +8,7 @@ export function ReactQueryProvider({ children }: PropsWithChildren) {
   return (
     <QueryClientProvider client={queryClient}>
       {children}
-      <ReactQueryDevtools initialIsOpen={false} />
+      {/* <ReactQueryDevtools initialIsOpen={false} /> */}
     </QueryClientProvider>
   );
 }
