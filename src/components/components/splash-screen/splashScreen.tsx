2b54f46906b95e180424ab51a6e03c378c6a449a
Jiiker <motive46@naver.com>
2024-05-29 19:33:55 +0900

    style: splashScreen UI 스타일 변경

--- src/components/components/splash-screen/splashScreen.tsx
+++ src/components/components/splash-screen/splashScreen.tsx
@@ -4,14 +4,52 @@ import Image from "next/image";
 
 export function SplashScreen() {
   return (
-    <div className="relative h-screen w-full opacity-100 transition-opacity duration-500 ease-in-out">
-      <div className="absolute bottom-0">
-        <Image
-          src="/images/splash.svg"
-          alt="splashIcon1"
-          width={390}
-          height={726}
-        />
+    <div className="relative h-screen w-full bg-[#252730]">
+      <div className="relative top-[60px] m-auto flex h-[500px] w-[326px] justify-center">
+        <div className="absolute bottom-0">
+          <Image
+            src="/images/splash-background.svg"
+            alt="splashIcon1"
+            width={266}
+            height={444}
+          />
+        </div>
+        <div className="absolute left-[5px] top-[186px]">
+          <Image
+            src="/images/splash-icon1.svg"
+            alt="splashIcon1"
+            width={170}
+            height={120}
+          />
+        </div>
+        <div className="absolute right-0 top-[281px]">
+          <Image
+            src="/images/splash-icon2.svg"
+            alt="splashIcon1"
+            width={160}
+            height={120}
+          />
+        </div>
+        <div className="absolute left-[8px] top-[27px]">
+          <Image
+            src="/images/splash-icon3.svg"
+            alt="splashIcon1"
+            width={180}
+            height={120}
+          />
+        </div>
+        <div className="absolute right-0 top-[105px]">
+          <Image
+            src="/images/splash-icon4.svg"
+            alt="splashIcon1"
+            width={160}
+            height={120}
+          />
+        </div>
+      </div>
+      <div className="absolute top-[584px] w-full text-center text-[24px] text-[#F7F7FA]">
+        <div>제주시 청년 정책</div>
+        <div>통합 플랫폼</div>
       </div>
     </div>
   );
