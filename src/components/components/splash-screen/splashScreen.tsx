3dd2ac563c9183ebe4ae43a1d4e8f46b564c1aef
Jiiker <motive46@naver.com>
2024-05-30 10:46:49 +0900

    feat: 애니메이션 반복 설정

--- src/components/components/splash-screen/splashScreen.tsx
+++ src/components/components/splash-screen/splashScreen.tsx
@@ -2,81 +2,117 @@
 
 import { motion } from "framer-motion";
 import Image from "next/image";
+import { useEffect, useState } from "react";
 
 export function SplashScreen() {
+  const [key, setKey] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setKey((prevKey) => prevKey + 1);
+    }, 3500);
+
+    return () => clearInterval(interval);
+  }, []);
+
   const container = {
-    hidden: { opacity: 1, scale: 0 },
-    visible: {
+    initial: {
+      opacity: 1,
+      scale: 0,
+    },
+    animate: {
       opacity: 1,
       scale: 1,
       transition: {
-        delayChildren: 0.3,
+        duration: 0.4,
+        delayChildren: 0.2,
         staggerChildren: 0.2,
+      },
+    },
+    exit: {
+      opacity: 0,
+      scale: 0,
+      transition: {
+        duration: 0.4,
       },
     },
   };
 
   const item = {
-    hidden: { y: 20, opacity: 0 },
-    visible: {
+    initial: {
+      y: 20,
+      opacity: 0,
+    },
+    animate: {
       y: 0,
       opacity: 1,
+    },
+    exit: {
+      y: 20,
+      opacity: 0,
     },
   };
 
   return (
     <div className="relative h-screen w-full bg-[#252730]">
       <motion.div
-        className="relative top-[60px] m-auto flex h-[500px] w-[326px] justify-center"
+        key={key}
+        className="relative top-[127px] m-auto flex h-[500px] w-[326px] justify-center"
         variants={container}
-        initial="hidden"
-        animate="visible"
+        initial="initial"
+        animate="animate"
+        exit="exit"
       >
         <motion.div className="absolute bottom-0" variants={item}>
           <Image
             src="/images/splash-background.svg"
-            alt="splashIcon1"
+            alt="splashBackground"
             width={266}
-            height={444}
+            height={416}
           />
         </motion.div>
-        <motion.div className="absolute left-[5px] top-[186px]" variants={item}>
+        <motion.div
+          className="absolute right-[8px] top-[301px]"
+          variants={item}
+        >
+          <Image
+            src="/images/splash-icon2.svg"
+            alt="splashIcon2"
+            width={150}
+            height={119}
+          />
+        </motion.div>
+        <motion.div className="absolute left-0 top-[219px]" variants={item}>
           <Image
             src="/images/splash-icon1.svg"
             alt="splashIcon1"
-            width={170}
-            height={120}
+            width={156}
+            height={113}
           />
         </motion.div>
-        <motion.div className="absolute right-0 top-[281px]" variants={item}>
-          <Image
-            src="/images/splash-icon2.svg"
-            alt="splashIcon1"
-            width={160}
-            height={120}
-          />
-        </motion.div>
-        <motion.div className="absolute left-[8px] top-[27px]" variants={item}>
-          <Image
-            src="/images/splash-icon3.svg"
-            alt="splashIcon1"
-            width={180}
-            height={120}
-          />
-        </motion.div>
-        <motion.div className="absolute right-0 top-[105px]" variants={item}>
+        <motion.div className="absolute right-0 top-[140px]" variants={item}>
           <Image
             src="/images/splash-icon4.svg"
-            alt="splashIcon1"
-            width={160}
-            height={120}
+            alt="splashIcon4"
+            width={159}
+            height={108}
           />
         </motion.div>
+        <motion.div className="absolute left-[22px] top-[48px]" variants={item}>
+          <Image
+            src="/images/splash-icon3.svg"
+            alt="splashIcon3"
+            width={159}
+            height={112}
+          />
+        </motion.div>
+        <motion.div
+          className="absolute top-[-9px] w-full text-center font-pretendard text-[22px] text-[#F7F7FA]"
+          variants={item}
+        >
+          제주시 청년 정책 통합 플랫폼
+        </motion.div>
       </motion.div>
-      <div className="absolute top-[584px] w-full text-center text-[24px] text-[#F7F7FA]">
-        <div>제주시 청년 정책</div>
-        <div>통합 플랫폼</div>
-      </div>
     </div>
   );
 }
