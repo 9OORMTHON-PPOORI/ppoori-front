ab7892b895249a6ed44f0ad7b6be2dcf36292ae3
Jiiker <motive46@naver.com>
2024-05-30 18:45:44 +0900

    style: 스플래시 화면 style 수정

--- src/components/components/splash-screen/splashScreen.tsx
+++ src/components/components/splash-screen/splashScreen.tsx
@@ -34,7 +34,7 @@ export function SplashScreen() {
   return (
     <div className="relative h-screen w-full bg-[#252730]">
       <motion.div
-        className="relative top-[127px] m-auto flex h-[500px] w-[326px] justify-center"
+        className="relative top-[15%] m-auto flex h-[500px] w-[326px] justify-center"
         variants={container}
         initial="initial"
         animate="animate"
@@ -48,26 +48,29 @@ export function SplashScreen() {
           />
         </motion.div>
         <motion.div
-          className="absolute right-[8px] top-[286px]"
+          className="absolute right-[4px] top-[301px]"
           variants={item}
         >
           <Image
             src="/images/splash-icon2.svg"
             alt="splashIcon2"
-            width={164}
-            height={128}
-          />
-        </motion.div>
-        <motion.div className="absolute left-0 top-[192px]" variants={item}>
-          <Image
-            src="/images/splash-icon1.svg"
-            alt="splashIcon1"
-            width={170}
-            height={136}
+            width={160}
+            height={125}
           />
         </motion.div>
         <motion.div
-          className="absolute right-[-6px] top-[100px]"
+          className="absolute left-[-6px] top-[212px]"
+          variants={item}
+        >
+          <Image
+            src="/images/splash-icon1.svg"
+            alt="splashIcon1"
+            width={168}
+            height={134}
+          />
+        </motion.div>
+        <motion.div
+          className="absolute right-[-6px] top-[110px]"
           variants={item}
         >
           <Image
@@ -77,16 +80,16 @@ export function SplashScreen() {
             height={141}
           />
         </motion.div>
-        <motion.div className="absolute left-[8px] top-[20px]" variants={item}>
+        <motion.div className="absolute left-[8px] top-[34px]" variants={item}>
           <Image
             src="/images/splash-icon4.svg"
             alt="splashIcon4"
-            width={172}
-            height={144}
+            width={168}
+            height={140}
           />
         </motion.div>
         <motion.div
-          className="absolute top-[-9px] w-full text-center font-pretendard text-[22px] text-[#F7F7FA]"
+          className="absolute top-[-9px] w-full text-center font-pretendard text-[22px] text-[#B4E0FF]/80"
           variants={item}
         >
           제주시 청년 정책 통합 플랫폼
