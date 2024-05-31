16163df721de159917aefbf9dff04d8076524226
Jiiker <motive46@naver.com>
2024-06-01 01:06:22 +0900

    feat: loading 화면 구현

---
+++ src/components/components/loading/loading.tsx
@@ -0,0 +1,33 @@
+"use client";
+
+import React from "react";
+import Lottie from "react-lottie-player";
+
+import Layout from "@/components/common/layout";
+
+import lottieJson from "../../../../public/lottie/loading-bubble.json";
+
+const Loading = () => {
+  return (
+    <Layout>
+      <div className="relative min-h-full w-full bg-[#F7F7FA]">
+        <div className="absolute top-[35%] h-[150px] w-full py-4">
+          <div className="flex w-full justify-center">
+            <Lottie
+              loop
+              animationData={lottieJson}
+              play
+              style={{ width: 60, height: 60 }}
+            />
+          </div>
+          <div className="mt-[16px] w-full text-title-4 text-[#858899]">
+            <div className="text-center">당신에게 딱맞는 정책을</div>
+            <div className="text-center">찾고 있어요</div>
+          </div>
+        </div>
+      </div>
+    </Layout>
+  );
+};
+
+export default Loading;
