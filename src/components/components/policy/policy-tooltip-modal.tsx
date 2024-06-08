25ef9aad94161e47ffb5f072e912cc663d69344f
Jiiker <motive46@naver.com>
2024-06-09 00:29:42 +0900

    refactor: 툴팁 모달 전체 이미지로 받아와서 직접 구현

--- src/components/components/policy/policy-tooltip-modal.tsx
+++ src/components/components/policy/policy-tooltip-modal.tsx
@@ -1,13 +1,6 @@
 import Image from "next/image";
 import React, { useState } from "react";
 
-import {
-  Tooltip,
-  TooltipContent,
-  TooltipProvider,
-  TooltipTrigger,
-} from "@/components/ui/tooltip";
-
 const TooltipModal = () => {
   const [isTooltipOpen, setIsTooltipOpen] = useState(false);
 
@@ -16,33 +9,29 @@ const TooltipModal = () => {
   };
 
   return (
-    <TooltipProvider>
-      <Tooltip open={isTooltipOpen} onOpenChange={handleTooltipChange}>
-        <TooltipTrigger asChild>
+    <div>
+      <Image
+        className="cursor-pointer"
+        src="/icon/helpIcon.svg"
+        alt="도움말 아이콘"
+        width={18}
+        height={18}
+        onClick={handleTooltipChange}
+        onMouseOver={() => setIsTooltipOpen(true)}
+        onMouseLeave={() => setIsTooltipOpen(false)}
+      />
+      {isTooltipOpen && (
+        <>
           <Image
-            className="cursor-pointer"
-            src="/icon/helpIcon.svg"
-            alt="도움말 아이콘"
-            width={18}
-            height={18}
-            onClick={handleTooltipChange}
-          />
-        </TooltipTrigger>
-        <TooltipContent side="bottom">
-          <Image
-            className="absolute bottom-20 right-0 z-50 animate-fadeIn"
-            src="/images/tooltip-triangle.svg"
+            className="absolute right-[-16px] top-[22px] z-50 duration-100 ease-in animate-in fade-in-0 zoom-in-90 slide-in-from-top-2"
+            src="/images/tooltips.svg"
             alt="툴팁"
-            width={80}
+            width={185}
             height={6}
           />
-          <p className="text-center text-[12px] font-extralight leading-[18px] tracking-[-0.1px] text-[#F0F0F5]">
-            현재 ChatGPT가 처리할 수 있는 범위 내에서 정책의 주요 내용을
-            요약했습니다.
-          </p>
-        </TooltipContent>
-      </Tooltip>
-    </TooltipProvider>
+        </>
+      )}
+    </div>
   );
 };
 
