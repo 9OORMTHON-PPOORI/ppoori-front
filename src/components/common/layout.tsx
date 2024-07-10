f9567ba13622dc8dbc16014ec0aa7ab26ba25806
S-jooyoung <jooyoung.dev@gmail.com>
2024-07-10 23:42:55 +0900

    Design: 배경 이미지 색상 변경
    - 청년 유형, 정책 카테고리 선택 페이지 배경 이미지 색상 변경
    - 정책 리스트 페이지 배경 이미지 색상 변경
    - 정책 상세 페이지 배경 이미지 색상 변경

--- src/components/common/layout.tsx
+++ src/components/common/layout.tsx
@@ -1,7 +1,7 @@
 export default function Layout(props: { children: React.ReactNode }) {
   return (
     <>
-      <main className="m-auto flex h-full w-full max-w-[390px] flex-grow items-center px-6">
+      <main className="m-auto flex h-full w-full max-w-[390px] flex-grow items-center bg-white px-6">
         {props.children}
       </main>
     </>
