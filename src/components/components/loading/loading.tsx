ea2ea8f232ca73bf6704e64f2f1c7839d90b157b
jooyoung <jooyoung.dev@gmail.com>
2026-07-27 03:31:05 +0900

    Fix: Lottie의 SSR 참조로 /policy 프리렌더가 실패하는 문제 해결
    react-lottie-player를 정적 import하면 의존 라이브러리인 lottie-web이
    모듈 평가 시점에 document로 스타일 태그를 삽입한다. 서버에는 document가
    없으므로 /policy 프리렌더가 ReferenceError로 중단되고, 결과적으로
    `next build`가 종료 코드 1로 실패했다.
    
      ReferenceError: document is not defined
      Error occurred prerendering page "/policy"
    
    next/dynamic의 ssr: false로 클라이언트에서만 로드하도록 바꾼다.
    스크립트 도착 전 레이아웃이 밀리지 않도록 래퍼 높이도 고정했다.
    
    이 수정으로 main 브랜치의 `next build`가 정상 종료된다.

--- src/components/components/loading/loading.tsx
+++ src/components/components/loading/loading.tsx
@@ -1,15 +1,23 @@
 "use client";
 
+import dynamic from "next/dynamic";
 import React from "react";
-import Lottie from "react-lottie-player";
 
 import lottieJson from "../../../../public/lottie/loading-bubble.json";
+
+/**
+ * react-lottie-player가 의존하는 lottie-web은 모듈 평가 시점에 document로
+ * 스타일 태그를 삽입한다. 서버에서 평가되면 "ReferenceError: document is not
+ * defined"로 프리렌더가 실패하므로 클라이언트에서만 로드한다.
+ */
+const Lottie = dynamic(() => import("react-lottie-player"), { ssr: false });
 
 const LoadingPresenter = () => {
   return (
     <div className="relative z-50 h-screen w-screen bg-[#F7F7FA]">
       <div className="absolute top-[35%] h-[150px] w-full py-4">
-        <div className="flex w-full justify-center">
+        {/* 스크립트가 늦게 도착해도 레이아웃이 밀리지 않도록 높이를 고정한다. */}
+        <div className="flex h-[60px] w-full justify-center">
           <Lottie
             loop
             animationData={lottieJson}
